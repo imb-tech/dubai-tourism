6a5fb5d75819b90dede6d5d9efb2f9e6aa7a60da
Ozodbek Abdisamatov <abdisamatovozodbek003@gmail.com>
2025-04-26 17:32:38 +0500

    card

--- src/views/home/products.tsx
+++ src/views/home/products.tsx
@@ -1,45 +1,29 @@
-import ProductCard from 'components/shared/product-card';
 import { ChevronRight } from 'lucide-react';
 import React from 'react';
+import CarCard from 'components/shared/car-card';
+import { cn } from 'lib/utils';
 
-const products: Product[] = [
-  {
-    id: 1,
-    name: 'Atraction ticketlar',
-    image: '/product.png',
-    price: 350.00,
-  },
-  {
-    id: 2,
-    name: 'Transfers xizmati',
-    image: '/product.png',
-    price: 350.00,
-  },
-  {
-    id: 3,
-    name: 'Mice group',
-    image: '/product.png',
-    price: 350.00,
-  },
-  {
-    id: 4,
-    name: 'VIP Concierge',
-    image: '/product.png',
-    price: 350.00,
-  },
-];
-
-export default function Products() {
+export default function Products({
+  data = [],
+  title = '',
+  className,
+}: {
+  data: Product[];
+  title: string;
+  className?: string;
+}) {
   return (
-    <div className='container mx-auto lg:px-0 px-3'>
-      <div className="flex items-center mb-5">
-        <h2 className="text-3xl font-semibold">Best sellers</h2>
-        <ChevronRight />
-      </div>
-      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-        {products.map((s) => (
-          <ProductCard key={s.id} {...s} />
-        ))}
+    <div className={cn(className)}>
+      <div className="container mx-auto lg:px-0 px-3">
+        <div className="flex items-center mb-5">
+          <h2 className="text-3xl font-semibold">{title}</h2>
+          <ChevronRight />
+        </div>
+        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+          {data.map((s) => (
+            <CarCard key={s.id} {...s} />
+          ))}
+        </div>
       </div>
     </div>
   );
